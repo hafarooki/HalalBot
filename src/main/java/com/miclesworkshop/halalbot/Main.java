d5cf7fc653e3330ee330a82293423b7147b7a813
miclebrick <miclebrick@outlook.com>
2018-11-29 22:47:53 -0500

    Added locking mechanism

--- src/main/java/com/miclesworkshop/halalbot/Main.java
+++ src/main/java/com/miclesworkshop/halalbot/Main.java
@@ -3,6 +3,10 @@ package com.miclesworkshop.halalbot;
 import org.apache.commons.cli.*;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.StandardOpenOption;
 
 public class Main {
     public static void main(String[] args) {
@@ -31,7 +35,29 @@ public class Main {
             System.out.println("Created data folder " + dataFolder.getPath());
         }
 
-        new HalalBot(dataFolder, token);
+
+        File lockFile = new File(dataFolder, "instance.lock");
+        try {
+            if (lockFile.createNewFile()) {
+                System.out.println("Created lock file " + lockFile.getPath());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (FileChannel channel = FileChannel.open(lockFile.toPath(), StandardOpenOption.WRITE)) {
+            try (FileLock lock = channel.tryLock()) {
+                if (lock == null) {
+                    System.out.println("Lock file is in use already!");
+                    System.exit(3);
+                }
+
+                new HalalBot(dataFolder, token);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(2);
+        }
     }
 
     private static void addOption(Options options, String opt, String description) {
